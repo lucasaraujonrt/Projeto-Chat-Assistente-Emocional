f4d8479ba6f47457d8d3e0af6d8236e5ca963870
Ciro-Lopes <ciro.flopes@gmail.com>
2020-05-19 19:21:35 -0300

    animacao butonToggle feita

--- chat_emocional/public/javascripts/myJs.js
+++ chat_emocional/public/javascripts/myJs.js
@@ -1,10 +1,13 @@
 function toggleButton() {
     var infoChat = document.getElementById("infoChat");
+    var chat = document.getElementById("chatToggle");
 
     if ($(infoChat).css('display') == 'none') {
-        infoChat.setAttribute('style', 'display:flex!important;');
+        infoChat.setAttribute('style', 'display:flex!important; -webkit-animation: slide-left .2s ease-out; -moz-animation: slide-left .2s ease-out;');
+        chat.setAttribute('style', 'display:none!important; -webkit-animation: slide-right .2s ease-out; -moz-animation: slide-right .2s ease-out;');
     }
     else {
-        infoChat.setAttribute('style', 'display:none!important;')
+        infoChat.setAttribute('style', '-webkit-animation: slide-right .2s ease-out; -moz-animation: slide-right .2s ease-out;');
+        chat.setAttribute('style', 'display:flex!important; -webkit-animation: slide-right .2s ease-out; -moz-animation: slide-right .2s ease-out;');
     }
 }
